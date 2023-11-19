03676eb5f670e8523f28fa7aea547aa857a95cfd
Abcapon <abcapon89@gmail.com>
2023-11-19 23:05:19 +0100

    ripristino redirect carrello

--- src/components/pages/confirm/Confirm.jsx
+++ src/components/pages/confirm/Confirm.jsx
@@ -1,30 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Confirm = () => {
 	const { token } = useParams();
 	const [confirmationMessage, setConfirmationMessage] = useState(null);
 	const navigate = useNavigate();
 
-	const location = useLocation();
-
-	const navigateToLogin = () => {
-		localStorage.setItem(`redirectPath`, location.pathname);
-		navigate("/login");
-	};
-
 	const confirmAccount = async () => {
 		try {
 			const response = await axios.get(
 				`${process.env.REACT_APP_SERVER_BASE_URL}/confirm/${token}`
 			);
-
-			navigateToLogin();
 			alert(
 				"Registrazione effettuata con successo, ora puoi effettuare il login"
 			);
+			navigate(`/login`);
 		} catch (error) {
 			setConfirmationMessage("Errore durante la conferma dell'account");
 			console.error("Errore durante la conferma dell'account:", error);
